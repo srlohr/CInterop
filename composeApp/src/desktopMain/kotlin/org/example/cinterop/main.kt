5df040b4031d7f1c4c7c4e457d4196419f141d68
srlohr <scott.lohr@yahoo.com>
2025-05-24 10:57:07 -0700

    Add the Kotlin CMP skeleton.

---
+++ composeApp/src/desktopMain/kotlin/org/example/cinterop/main.kt
@@ -0,0 +1,13 @@
+package org.example.cinterop
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "CInterop",
+    ) {
+        App()
+    }
+}
